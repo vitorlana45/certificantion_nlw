87ddc63210b027f1da8f993c83ad1502a9213a47
vitorlana45 <vitoresmerio00@gmail.com>
2024-02-06 19:35:15 -0300

    Projeto criado

---
+++ src/main/java/com/vitorlana/certificantion_nlw/CertificantionNlwApplication.java
@@ -0,0 +1,13 @@
+package com.vitorlana.certificantion_nlw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificantionNlwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificantionNlwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vitorlana/certificantion_nlw/CertificantionNlwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitorlana.certificantion_nlw;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificantionNlwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
